36f3d64696fdc11f9314f81998efa9a078786592
MishaNikasov <mishanikasov@mail.ru>
2019-09-16 16:40:46 +0300

    Initial commit

---
+++ app/src/main/java/com/nikasov/mynote/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nikasov.mynote.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nikasov.mynote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/nikasov/mynote/view/fragment/MainFragment.java
@@ -0,0 +1,27 @@
+package com.nikasov.mynote.view.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nikasov.mynote.R;
+
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+}
